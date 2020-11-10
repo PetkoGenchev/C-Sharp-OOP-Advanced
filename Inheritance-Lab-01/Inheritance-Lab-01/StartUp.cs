fc54a4283b80eb45ce5dd8b528ac1419fb0241a0
PetkoGenchev <petko.genchev@yahoo.com>
2020-11-10 16:56:29 +0200

    inheritance02

--- Inheritance-Lab-01/Inheritance-Lab-01/StartUp.cs
+++ Inheritance-Lab-01/Inheritance-Lab-01/StartUp.cs
@@ -7,8 +7,14 @@ namespace Farm
         static void Main(string[] args)
         {
             Dog dog = new Dog();
+            dog.Eat();
             dog.Bark();
-            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+
+
 
         }
     }
