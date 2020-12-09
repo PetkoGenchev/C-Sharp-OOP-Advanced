86b5be2b1abd32f84fe4bc97fc6de1c2a78a6899
PetkoGenchev <petko.genchev@yahoo.com>
2020-12-09 22:45:13 +0200

    AbstractionWork01

---
+++ InterfaceAndAbstraction/InterfaceAndAbstraction-Lab-02/Seat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+
+        public string Start()
+        {
+            return $"{this.Color} Seat {this.Model}\r Engine start";
+
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+    }
+}
---
+++ InterfaceAndAbstraction/InterfaceAndAbstraction-Lab-02/Tesla.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Start()
+        {
+            return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries\r Engine start";
+
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+    }
+}
