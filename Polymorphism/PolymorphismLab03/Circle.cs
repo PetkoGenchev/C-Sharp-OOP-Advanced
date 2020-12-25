6fcf0a80ebf0bd56864236138a053b5e49874753
petkogenchev <petkogenchev@SOF-PETGEN-MOB>
2020-12-25 18:07:36 +0200

    Polymorphism04

---
+++ Polymorphism/PolymorphismLab03/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    class Circle : Shape
+    {
+        public double Radius { get; private set; }
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Sqrt(this.Radius);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw();
+        }
+    }
+}
